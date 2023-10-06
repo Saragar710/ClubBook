240efc997b5536ff6ee47d053e327d9e3cf0982b
Sara Garcia <saragca81@gmail.com>
2023-10-06 15:22:22 -0700

    added to css and engine to server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
 const route = require("./controllers");
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const sequelize = require("./config/connection");
 const { Sequelize } = require("sequelize");
@@ -10,6 +12,9 @@ const SequelizeStore = require("connect-seesion-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 const sess = {
     secret: process.env.SESSIONSECRET,
