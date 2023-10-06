0d7eaba732fb35433e0f77fe2fa546a6f996448b
Daniel Polk <Danielpolk7@icloud.com>
2023-10-05 20:49:42 -0400

    added express, dotenv, and session to server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("Program started..." )
+require("dotenv").config();
+
+const express = require("express");
+const session = require("express-session");
+const route = require("./controllers");
+
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-seesion-sequelize")(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
