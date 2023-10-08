1303b9ab29b1669210df57e7ac3ee8ae5383459f
Sara Garcia <saragca81@gmail.com>
2023-10-07 19:23:55 -0700

    added connect-session sequelizer

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ require("dotenv").config();
 
 const express = require("express");
 const session = require("express-session");
-const route = require("./controllers");
+ const route = require("./controllers");
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
 const sequelize = require("./config/connection");
 const { Sequelize } = require("sequelize");
-const SequelizeStore = require("connect-seesion-sequelize")(session.Store);
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
