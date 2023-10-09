71daa9d121ba05e98fb73c1fa1fdf44a724e28bb
Sara Garcia <saragca81@gmail.com>
2023-10-09 11:03:40 -0700

    added css

--- utils/helper.js
+++ utils/helper.js
@@ -12,11 +12,11 @@ module.exports = {
   
       // Return a random emoji
       if (randomNum > 0.7) {
-        return `<span for="img" aria-label="shark">💡</span>`;
+        return `<span for="img" aria-label="shark">129416</span>`;
       } else if (randomNum > 0.4) {
-        return `<span for="img" aria-label="golf">💻</span>`;
+        return `<span for="img" aria-label="golf">9971</span>`;
       } else {
-        return `<span for="img" aria-label="beach-yoga">⚙️</span>`;
+        return `<span for="img" aria-label="beach-yoga">1F9D8</span>`;
       }
     },
   };
