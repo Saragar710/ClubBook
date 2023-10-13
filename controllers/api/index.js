d8858dae95cd917322db0f9a08c15cc9c53c835a
Sara Garcia <saragca81@gmail.com>
2023-10-12 18:05:21 -0700

    deleted routes directory

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./clubRoutes');
+const clubRoutes = require('./clubRoutes');
 
 router.use('/users', userRoutes);
 router.use('/clubs', clubRoutes);
