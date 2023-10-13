3a1fee6e785f60ca8d061417f79b84dac02ba78f
Sara Garcia <saragca81@gmail.com>
2023-10-12 19:38:56 -0700

    updated config

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,35 @@
-require('dotenv').config();
+// require('dotenv').config();
+// const Sequelize = require('sequelize');
+
+
+// // let sequelize = new Sequelize(process.env.MYSQLURI);
+
+// let sequelize = {
+//     "development": {
+//         "username": process.env.DATABASE_USER,
+//         "password": process.env.DATABASE_PASSWORD,
+//         "database": process.env.DATABASE_NAME,
+//         "host": process.env.DATABASE_HOST,
+//         "dialect": 'postgres',
+//         }
+// }
+
+// module.exports = sequelize;
 const Sequelize = require('sequelize');
-
-
-let sequelize = new Sequelize(process.env.MYSQLURI);
-
-
-
+require('dotenv').config();
+let sequelize;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
 module.exports = sequelize;
