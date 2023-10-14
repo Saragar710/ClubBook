bdc8452797fe55fc3fd3309b292a98c60eb9c12c
Daniel Polk <Danielpolk7@icloud.com>
2023-10-13 20:37:39 -0400

    added routes for user and clubs

---
+++ api/userRoutes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const User = require('../models/User');
+
+router.post('/', async (req, res) => {
+    try {
+      const userData = await User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+      });
+      res.status(200).json(userData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
