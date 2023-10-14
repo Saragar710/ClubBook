75e37583b3808249bdaaf8a789513019e136b113
Marcus Matthews <mlmatthews75@gmail.com>
2023-10-14 18:09:56 -0400

    added carousel code to main html and created js code to randomize the images in main html carousel portion of code.

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,1 +1,9 @@
-const carousel = new bootstrap.Carousel('#myCarousel');
+var imageUrls = ["/images/beach-yoga.avif", "/images/golf club.avif", "/images/bookclub.avif", "/images/sharks.avif", "/images/wine club.avif"];
+
+function updateCarousel() {
+  var randomImageUrl = imageUrls[Math.floor(Math.random() * imageUrls.length)];
+
+  document.querySelector("#myCarousel .carousel-inner .carousel-item:first-child img").src = randomImageUrl;
+}
+
+window.onload = updateCarousel;
