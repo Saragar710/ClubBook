e84cd45099e1e00b9b185fc28a62a8826e81f975
Sara Garcia <saragca81@gmail.com>
2023-10-15 10:38:06 -0700

    added scripts for html pages

--- public/js/login.js
+++ public/js/login.js
@@ -3,20 +3,20 @@ const loginFormHandler = async (event) => {
   //defining username (email) and password from the login html
 
     // Collect values from the login form
-    const email = document.querySelector('#email-login').value.trim();
+    const userName = document.querySelector('#userName-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
   
-    if (email && password) {
+    if (userName && password) {
       // Send POST request to the API endpoint
       const response = await fetch('/api/users/login', {
         method: 'POST',
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify({ userName, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   //if ok, go to file.html
       if (response.ok) {
         // When successful, redirect to the profile page
-        document.location.replace('/profile');
+        document.location.replace('/club');
       } else {
         alert(response.statusText);
       }
@@ -26,19 +26,19 @@ const loginFormHandler = async (event) => {
   const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    const name = document.querySelector('#name-signup').value.trim();
+    const name = document.querySelector('#userName-signup').value.trim();
     //const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
   
     if (name && email && password) {
       const response = await fetch('/api/users', {
         method: 'POST',
-        body: JSON.stringify({ name, email, password }),
+        body: JSON.stringify({ userNamename, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
       if (response.ok) {
-        document.location.replace('/profile');
+        document.location.replace('/club');
       } else {
         alert(response.statusText);
       }
