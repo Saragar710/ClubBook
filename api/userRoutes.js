cb50a2cb1ce3e125079920377acc37c2cbac5fa1
Sara Garcia <saragca81@gmail.com>
2023-10-14 18:35:14 -0700

    commented out profile.js, lines in routes.js

--- api/userRoutes.js
+++ api/userRoutes.js
@@ -5,7 +5,7 @@ router.post('/', async (req, res) => {
     try {
       const userData = await User.create({
         username: req.body.username,
-        email: req.body.email,
+        // email: req.body.email,
         password: req.body.password,
       });
       res.status(200).json(userData);
---
+++ public/js/club.js
@@ -0,0 +1,47 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const name = document.querySelector('#club-name').value.trim();
+   
+    const description = document.querySelector('#club-desc').value.trim();
+  
+    if (name && needed_funding && description) {
+      const response = await fetch(`/api/clubs`, {
+        method: 'POST',
+        body: JSON.stringify({ name, description }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/club');
+      } else {
+        alert('Unable to create club');
+      }
+    }
+  };
+  
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/clubs/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/club');
+      } else {
+        alert('Failed to delete club');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.new-club-form')
+    .addEventListener('submit', newFormHandler);
+  
+  document
+    .querySelector('.club-list')
+    .addEventListener('click', delButtonHandler);
