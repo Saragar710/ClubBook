8d05369ab22232795c60b3475a4b0165a571a57b
Mike Reilly <mikereilly5@gmail.com>
2023-10-16 00:08:56 -0400

    Restored Session on server for user

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-
+const session = require('express-session');
 const express = require("express");
 const route = require("./controllers");
 
@@ -7,6 +7,19 @@ const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+
+const sess = {
+    secret: procces.env.SESSIONSECRET,
+    cookie: {
+      maxAge: 300000,
+      httpOnly: true,
+      secure: false,
+      sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true    
+  };
 
 app.use(express.static("public"));
 app.use(express.static("views"));
